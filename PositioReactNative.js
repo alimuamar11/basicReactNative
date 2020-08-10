9f26bda2ef876310f1fbbc5710c1ae50b8ab5d03
alimuamar11 <alimuamar11@gmail.com>
2020-08-10 07:22:05 +0700

    positio

--- PositioReactNative.js
+++ PositioReactNative.js
@@ -4,9 +4,12 @@ import keranjang from './cart.png';
 
 const PositioReactNative = () => {
     return (
-        <View>
-            <Image source={keranjang} style={styles.cartIcon}/>
-            <Text>Materi Position</Text>
+        <View style={styles.wrapper}>
+            <View style={styles.cartWrapper}>
+                  <Image source={keranjang} style={styles.cartIcon}/>
+                  <Text style={styles.notif}>10</Text>
+            </View>
+            <Text style={styles.textKeranjang}>Keranjang Belanja</Text>
         </View>
     )
 }
@@ -14,8 +17,40 @@ const PositioReactNative = () => {
 export default PositioReactNative
 
 const styles = StyleSheet.create({
+    wrapper: {
+        padding: 20,
+        alignItems: 'center'
+    },
     cartIcon: {
         width: 50,
         height: 50
     },
+    cartWrapper: {
+        borderWidth: 1,
+        borderColor: '#4398d1',
+        width: 93,
+        height: 93,
+        borderRadius: 93/2,
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'relative' //agar tidak keluar dari komponen ini
+    },
+    notif: {
+        fontSize: 12,
+        color: 'white',
+        backgroundColor: '#6fcf97',
+        padding: 4,
+        borderRadius: 25,
+        width: 24,
+        height: 24,
+        position: 'absolute', //agar bebes bergerak ,kemudian
+        top: 0,
+        right: 0
+    },
+    textKeranjang: {
+        fontSize: 15,
+        color: '#777777',
+        marginTop: 15,
+        fontWeight: 'bold'
+    }
 })
